be1355ae7e645c8162bdc5fcb420830e9537f6f4
Deepak Verma <deepak.verma@jabong.com>
2015-02-08 16:49:49 +0530

    First commit to my repository

---
+++ JaiDadaBudhe/src/com/vishitech/jai/dada/budhe/MyActivity.java
@@ -0,0 +1,15 @@
+package com.vishitech.jai.dada.budhe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
